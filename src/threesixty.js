1022fa90b39483d88bd78d5710cb2a30acd18c8e
Mladen Ilić <mladen@stuntcoders.com>
2016-08-30 23:42:36 +0200

    Implement play feature

--- src/threesixty.js
+++ src/threesixty.js
@@ -1,13 +1,22 @@
-var ThreeSixty = (function () {
+var ThreeSixty = (function (window) {
 
 	return function (container, options) {
 		var self = this;
 		var index = 0;
 
+		var loopTimeoutId = null;
+		var looping = false;
+		var loop = function (reversed = false) {
+			reversed ? self.prev() : self.next();
+
+			loopTimeoutId = window.setTimeout(loop, options.speed);
+		};
+
 		options.width = options.width || 300;
 		options.height = options.height || 300;
 		options.count = options.count || 0;
 		options.perRow = options.perRow || 0;
+		options.speed = options.speed || 100;
 
 		container.style.width = options.width + 'px';
 		container.style.height = options.height + 'px';
@@ -16,13 +25,11 @@ var ThreeSixty = (function () {
 
 		self.next = function () {
 			index = index + 1 > options.count - 1 ? 0 : index + 1;
-			console.log(index);
 			self.update();
 		};
 
 		self.prev = function () {
 			index = index - 1 < 0 ? options.count - 1 : index - 1;
-			console.log(index);
 			self.update();
 		};
 
@@ -30,5 +37,23 @@ var ThreeSixty = (function () {
 			container.style.backgroundPositionX = -(index % options.perRow) * options.width + 'px';
 			container.style.backgroundPositionY = -Math.floor(index / options.perRow) * options.height + 'px';
 		};
+
+		self.play = function (reversed) {
+			if (looping) {
+				return;
+			}
+
+			loop();
+			looping = true;
+		};
+
+		self.stop = function () {
+			if (!looping) {
+				return;
+			}
+			
+			window.clearTimeout(loopTimeoutId);
+			looping = false;
+		}
 	};
-} ());
+} (window));
