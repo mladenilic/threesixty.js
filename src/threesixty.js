654ff88cf2b1535c5345ad0bedb30cd44b5e71c9
Mladen Ilic <mladenilic480@gmail.com>
2019-06-29 12:41:25 +0200

    Rewrite threesixty using ES6+

--- src/threesixty.js
+++ src/threesixty.js
@@ -1,211 +1,212 @@
-var ThreeSixty = (function (window) {
-    'use strict';
+class ThreeSixty {
+    constructor(container, options) {
+        this.container = container;
 
-    return function (container, options) {
-        var self = this;
-        var index = 0;
+        this.options = Object.assign({
+            width: 300,
+            height: 300,
+            count: 0,
+            perRow: 0,
+            speed: 100,
+            dragTolerance: 10,
+            swipeTolerance: 10,
+            draggable: true,
+            swipeable: true,
+            keys: true,
+            prev: true,
+            next: true,
+            inverted: false
+        }, options);
 
-        var loopTimeoutId = null;
-        var looping = false;
-        var dragOrigin = false;
-        var loop = function (reversed) {
-            reversed ? self.prev() : self.next();
+        this.index = 0;
 
-            loopTimeoutId = window.setTimeout(function () {
-                loop(reversed);
-            }, options.speed);
-        };
+        this.loopTimeoutId = null;
+        this.looping = false;
 
-        var eventHanlers = {
+        this.dragOrigin = null;
+
+        this.eventHandlers = {
             container: {
-                mousedown: function (e) {
-                    dragOrigin = e.pageX;
-                },
-                touchstart: function (e) {
-                    dragOrigin = e.touches[0].clientX;
-                },
-                touchend: function () {
-                    dragOrigin = false;
-                }
+                mousedown: (e) => this.dragOrigin = e.pageX,
+                touchstart: (e) => this.dragOrigin = e.touches[0].clientX,
+                touchend: () => this.dragOrigin = null,
             },
             prev: {
-                mousedown: function (e) {
+                mousedown: (e) => {
                     e.preventDefault();
-                    self.play(true);
+                    this.play(true);
                 },
-                mouseup: function (e) {
+                mouseup: (e) => {
                     e.preventDefault();
-                    self.stop();
+                    this.stop();
                 },
-                touchstart: function (e) {
+                touchstart: (e) => {
                     e.preventDefault();
-                    self.prev();
+                    this.prev();
                 }
             },
             next: {
-                mousedown: function (e) {
+                mousedown: (e) => {
                     e.preventDefault();
-                    self.play();
+                    this.play();
                 },
-                mouseup: function (e) {
+                mouseup: (e) => {
                     e.preventDefault();
-                    self.stop();
+                    this.stop();
                 },
-                touchstart: function (e) {
+                touchstart: (e) => {
                     e.preventDefault();
-                    self.next();
+                    this.next();
                 }
             },
-            document: {
-                mouseup: function () {
-                    dragOrigin = false;
-                },
-                mousemove: function (e) {
-                    if (dragOrigin && Math.abs(dragOrigin - e.pageX) > options.dragTolerance) {
-                        self.stop();
-                        dragOrigin > e.pageX ? self.prev() : self.next();
-                        dragOrigin = e.pageX;
+            global: {
+                mouseup: () => this.dragOrigin = null,
+                mousemove: (e) => {
+                    if (this.dragOrigin && Math.abs(this.dragOrigin - e.pageX) > this.options.dragTolerance) {
+                        this.stop();
+                        this.dragOrigin > e.pageX ? this.prev() : this.next();
+                        this.dragOrigin = e.pageX;
                     }
                 },
-                touchmove: function (e) {
-                    if (dragOrigin && Math.abs(dragOrigin - e.touches[0].clientX) > options.swipeTolerance) {
-                        self.stop();
-                        dragOrigin > e.touches[0].clientX ? self.prev() : self.next();
-                        dragOrigin = e.touches[0].clientX;
+                touchmove: (e) => {
+                    if (this.dragOrigin && Math.abs(this.dragOrigin - e.touches[0].clientX) > this.options.swipeTolerance) {
+                        this.stop();
+                        this.dragOrigin > e.touches[0].clientX ? this.prev() : this.next();
+                        this.dragOrigin = e.touches[0].clientX;
                     }
                 },
-                keydown: function (e) {
+                keydown: (e) => {
                     if ([37, 39].includes(e.keyCode)) {
-                        self.play(37 === e.keyCode);
+                        this.play(37 === e.keyCode);
                     }
                 },
-                keyup: function (e) {
+                keyup: (e) => {
                     if ([37, 39].includes(e.keyCode)) {
                         self.stop();
                     }
                 }
             }
-        }
-
-        options.width = options.width || 300;
-        options.height = options.height || 300;
-        options.count = options.count || 0;
-        options.perRow = options.perRow || 0;
-        options.speed = options.speed || 100;
-        options.dragTolerance = options.dragTolerance || 10;
-        options.swipeTolerance = options.dragTolerance || 10; 
-        options.draggable = options.hasOwnProperty("draggable") ? options.draggable : true;
-        options.swipeable = options.hasOwnProperty("swipeable") ? options.swipeable : true;
-        options.keys = options.hasOwnProperty("keys") ? options.keys : true;        
-        options.prev = options.prev || false;
-        options.next = options.next || false;
-        options.inverted = options.inverted || false;
-
-        container.style.width = options.width + 'px';
-        container.style.height = options.height + 'px';
-        container.style.backgroundImage = 'url("' + options.image + '")';
-        container.style.backgroundPosition = '0 0';
-        container.style.backgroundSize = (options.perRow * 100) + '%';
-
-        if (options.draggable) {
-            container.addEventListener('mousedown', eventHanlers.container.mousedown);
-            document.addEventListener('mouseup', eventHanlers.document.mouseup);
-            document.addEventListener('mousemove', eventHanlers.document.mousemove);
-        }
-
-        if (options.swipeable) {
-            container.addEventListener('touchstart', eventHanlers.container.touchstart);
-            container.addEventListener('touchend', eventHanlers.container.touchend);
-            document.addEventListener('touchmove', eventHanlers.document.touchmove);
-        }
-
-        if (options.keys) {
-            document.addEventListener('keydown', eventHanlers.document.keydown);
-            document.addEventListener('keyup', eventHanlers.document.keyup);
-        }
-
-        if (options.prev) {
-            options.prev.addEventListener('mousedown', eventHanlers.prev.mousedown);
-            options.prev.addEventListener('mouseup', eventHanlers.prev.mouseup);
-            options.prev.addEventListener('touchstart', eventHanlers.prev.touchstart);
-        }
-
-        if (options.next) {
-            options.next.addEventListener('mousedown', eventHanlers.next.mousedown);
-            options.next.addEventListener('mouseup', eventHanlers.next.mouseup);
-            options.next.addEventListener('touchstart', eventHanlers.next.touchstart);
-        }
-
-        self.next = function () {
-            self.goTo(options.inverted ? index + 1 : index - 1);
-            self.update();
         };
 
-        self.prev = function () {
-            self.goTo(options.inverted ? index - 1 : index + 1);
-            self.update();
-        };
+        this.initContainer();
+        this.initEvents();
+    }
 
-        self.goTo = function (newIndex) {
-            newIndex = newIndex > options.count - 1 ? 0 : newIndex;
-            index = newIndex < 0 ? options.count - 1 : newIndex;
+    initContainer() {
+        this.container.style.width = this.options.width + 'px';
+        this.container.style.height = this.options.height + 'px';
+        this.container.style.backgroundImage = `url("${this.options.image}")`;
+        this.container.style.backgroundPosition = '0 0';
+        this.container.style.backgroundSize = (this.options.perRow * 100) + '%';
+    }
 
-            self.update();
-        };
-
-        self.update = function () {
-            container.style.backgroundPositionX = -(index % options.perRow) * options.width + 'px';
-            container.style.backgroundPositionY = -Math.floor(index / options.perRow) * options.height + 'px';
-        };
-
-        self.play = function (reversed) {
-            if (looping) {
-                return;
-            }
-
-            loop(reversed);
-            looping = true;
-        };
-
-        self.stop = function () {
-            if (!looping) {
-                return;
-            }
-
-            window.clearTimeout(loopTimeoutId);
-            looping = false;
-        };
-
-        self.destroy = function () {
-            self.stop();
-
-            container.removeEventListener('mousedown', eventHanlers.container.mousedown);
-            container.removeEventListener('touchstart', eventHanlers.container.touchstart);
-            container.removeEventListener('touchend', eventHanlers.container.touchend);
-
-            document.removeEventListener('mouseup', eventHanlers.document.mouseup);
-            document.removeEventListener('mousemove', eventHanlers.document.mousemove);
-            document.removeEventListener('touchmove', eventHanlers.document.touchmove);
-            document.removeEventListener('keydown', eventHanlers.document.keydown);
-            document.removeEventListener('keyup', eventHanlers.document.keyup);
-
-            if (options.prev) {
-                options.prev.removeEventListener('mousedown', eventHanlers.prev.mousedown);
-                options.prev.removeEventListener('mouseup', eventHanlers.prev.mouseup);
-                options.prev.removeEventListener('touchstart', eventHanlers.prev.touchstart);
-            }
-
-            if (options.next) {
-                options.next.removeEventListener('mousedown', eventHanlers.next.mousedown);
-                options.next.removeEventListener('mouseup', eventHanlers.next.mouseup);
-                options.next.removeEventListener('touchstart', eventHanlers.next.touchstart);
-            }
-
-            container.style.width = '';
-            container.style.height = '';
-            container.style.backgroundImage = '';
-            container.style.backgroundPosition = '';
-            container.style.backgroundSize = '';
+    initEvents() {
+        if (this.options.draggable) {
+            this.container.addEventListener('mousedown', this.eventHandlers.container.mousedown);
+            global.addEventListener('mouseup', this.eventHandlers.global.mouseup);
+            global.addEventListener('mousemove', this.eventHandlers.global.mousemove);
         }
-    };
-} (window));
+
+        if (this.options.swipeable) {
+            this.container.addEventListener('touchstart', this.eventHandlers.container.touchstart);
+            this.container.addEventListener('touchend', this.eventHandlers.container.touchend);
+            global.addEventListener('touchmove', this.eventHandlers.global.touchmove);
+        }
+
+        if (this.options.keys) {
+            global.addEventListener('keydown', this.eventHandlers.global.keydown);
+            global.addEventListener('keyup', this.eventHandlers.global.keyup);
+        }
+
+        if (this.options.prev) {
+            this.options.prev.addEventListener('mousedown', this.eventHandlers.prev.mousedown);
+            this.options.prev.addEventListener('mouseup', this.eventHandlers.prev.mouseup);
+            this.options.prev.addEventListener('touchstart', this.eventHandlers.prev.touchstart);
+        }
+
+        if (this.options.next) {
+            this.options.next.addEventListener('mousedown', this.eventHandlers.next.mousedown);
+            this.options.next.addEventListener('mouseup', this.eventHandlers.next.mouseup);
+            this.options.next.addEventListener('touchstart', this.eventHandlers.next.touchstart);
+        }
+    }
+
+    next() {
+        this.goto(this.options.inverted ? this.index + 1 : this.index - 1);
+    }
+
+    prev() {
+        this.goto(this.options.inverted ? this.index - 1 : this.index + 1);
+    }
+
+    goto(index) {
+        this.index = (this.options.count + index) % this.options.count;
+
+        this.update();
+    }
+
+    loop(reversed) {
+        reversed ? this.prev() : this.next();
+
+        this.loopTimeoutId = global.setTimeout(() => {
+            this.loop(reversed);
+        }, this.options.speed);
+    }
+
+    play (reversed) {
+        if (this.looping) {
+            return;
+        }
+
+        this.loop(reversed);
+        this.looping = true;
+    }
+
+    stop () {
+        if (!this.looping) {
+            return;
+        }
+
+        global.clearTimeout(this.loopTimeoutId);
+        this.looping = false;
+    }
+
+    update () {
+        this.container.style.backgroundPositionX = -(this.index % this.options.perRow) * this.options.width + 'px';
+        this.container.style.backgroundPositionY = -Math.floor(this.index / this.options.perRow) * this.options.height + 'px';
+    }
+
+    destroy() {
+        this.stop();
+
+        this.container.removeEventListener('mousedown', this.eventHandlers.container.mousedown);
+        this.container.removeEventListener('touchstart', this.eventHandlers.container.touchstart);
+        this.container.removeEventListener('touchend', this.eventHandlers.container.touchend);
+
+        global.removeEventListener('mouseup', this.eventHandlers.global.mouseup);
+        global.removeEventListener('mousemove', this.eventHandlers.global.mousemove);
+        global.removeEventListener('touchmove', this.eventHandlers.global.touchmove);
+        global.removeEventListener('keydown', this.eventHandlers.global.keydown);
+        global.removeEventListener('keyup', this.eventHandlers.global.keyup);
+
+        if (this.options.prev) {
+            this.options.prev.removeEventListener('mousedown', this.eventHandlers.prev.mousedown);
+            this.options.prev.removeEventListener('mouseup', this.eventHandlers.prev.mouseup);
+            this.options.prev.removeEventListener('touchstart', this.eventHandlers.prev.touchstart);
+        }
+
+        if (this.options.next) {
+            this.options.next.removeEventListener('mousedown', this.eventHandlers.next.mousedown);
+            this.options.next.removeEventListener('mouseup', this.eventHandlers.next.mouseup);
+            this.options.next.removeEventListener('touchstart', this.eventHandlers.next.touchstart);
+        }
+
+        this.container.style.width = '';
+        this.container.style.height = '';
+        this.container.style.backgroundImage = '';
+        this.container.style.backgroundPosition = '';
+        this.container.style.backgroundSize = '';
+    }
+}
+
+export default ThreeSixty;
