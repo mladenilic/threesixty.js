ccf3620354f5fb99e1a050bfef563aa0e482d9ab
Mladen Ilic <mladenilic480@gmail.com>
2019-06-29 19:27:41 +0200

    Add basic test suite

---
+++ test/basic.test.js
@@ -0,0 +1,39 @@
+const expect = require('chai').expect;
+const ThreeSixty = require('../dist/threesixty');
+
+describe('Basic Features', () => {
+  before(() => {
+    this.container = document.getElementById('threesixty');
+    this.threesixty = new ThreeSixty(document.getElementById('threesixty'), {
+        image: 'https://s3.eu-central-1.amazonaws.com/threesixty.js/watch.jpg',
+        width: 320,
+        height: 320,
+        count: 31,
+        perRow: 4
+    });
+  });
+
+  it('should set container properties', () => {
+    expect(this.container.style.backgroundImage).to.be.equal('url(https://s3.eu-central-1.amazonaws.com/threesixty.js/watch.jpg)');
+    expect(this.container.style.backgroundSize).to.be.equal('400%');
+  });
+
+  it('should be in sprite mode when single image is provided', () => {
+     expect(this.threesixty.sprite).to.be.equal(true);
+  });
+
+  it('should update current index when navigation is used', () => {
+    expect(this.threesixty.index).to.be.equal(0);
+
+    this.threesixty.next();
+    this.threesixty.next();
+    this.threesixty.next();
+
+    expect(this.threesixty.index).to.be.equal(3);
+
+    this.threesixty.prev();
+    this.threesixty.prev();
+
+    expect(this.threesixty.index).to.be.equal(1);
+  });
+});
